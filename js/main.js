f45a0d70c5c6782b49fb51fe02840dc1b456bd66
leejaejeong <jaejeonglee6@gmail.com>
2024-06-21 14:13:45 +0900

    헤더 스크롤 시 고정효과 및 컬렉션 섹션 추가

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    
+    let header_offset = $('.header_container').offset().top;
+    
+    if($(window).scrollTop() === 0){
+        $('.header_container').removeClass('fix');
+    }else if($(window).scrollTop() >= header_offset){
+        $('.header_container').addClass('fix');
+    }
+})
